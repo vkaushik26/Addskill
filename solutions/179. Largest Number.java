f161204e39c2f55b2a5b5490c7c5338cf9d26f60
vaibhav kaushik <kaushikvaibhav26@gmail.com>
2020-11-08 02:00:27 +0530

    5 ms

---
+++ solutions/179. Largest Number.java
@@ -0,0 +1,29 @@
+class Solution {
+    
+    private class LargerNumberComparator implements Comparator<String> {
+        @Override
+        public int compare(String a, String b) {
+            String order1 = a + b;
+            String order2 = b + a;
+           return order2.compareTo(order1);
+        }
+    }
+    public String largestNumber(int[] nums) {
+       String[] arr = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(arr, new LargerNumberComparator());
+​
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+​
+        String largestNumberStr = new String();
+        for (String numArr : arr) {
+            largestNumberStr += numArr;
+        }
+​
+        return largestNumberStr; 
+    }
+}
